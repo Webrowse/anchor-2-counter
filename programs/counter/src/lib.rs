0a6c73b9acf756758f59ef7105195bbca85d21ad
Webrowse <great.adarsh@gmail.com>
2025-11-03 23:54:06 +0530

    unsafe counter program

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,16 +1,66 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EF4Zr3vXtAwcfQR2EeGqiGeQ5jcfXXGXJKcdkvjHQPJ5");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod counter {
+pub mod counter_program {
+    use crate::instruction::Initialize;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<InitializeCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter_account;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<ModifyCounter>) -> Result<()>{
+        let counter = &mut ctx.accounts.counter_account;
+        counter.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<ModifyCounter>) -> Result<()>{
+        let counter = &mut ctx.accounts.counter_account;
+        require!(counter.count > 0, CustomError::CounterUnderflow);
+        counter.count -= 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeCounter<'info> {
+    #[account(
+        init, payer = user, 
+        space = 8 + 8,
+        seeds = [b"counter", user.key().as_ref()],
+        bump    
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ModifyCounter<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter", user.key().as_ref()],
+        bump
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+    
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct CounterAccount   {
+    pub count: u64,
+}
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Counter cannot be below zero.")]
+    CounterUnderflow,
+}
